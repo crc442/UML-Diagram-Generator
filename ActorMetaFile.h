81ea5d98c33477aa0e12b4b2208e453ba95e3e3a
crc442 <EequalsMCsquare91@gmail.com>
2014-04-25 15:39:55 -0500

    some corrections

--- ActorMetaFile.h
+++ ActorMetaFile.h
@@ -6,6 +6,42 @@
 #include "ActDlgProc.h"
 #include "CmptDlgProc.h"
 
+static int linecnt,ellipsecnt,rectcnt,actorcnt,fcirclecnt;
+
+	static RECT lineRect[10];
+	static RECT ellipseRect[10],fcircleRect[10];
+	static RECT rectRect[10];
+	static POINT actorPt[10];
+
+
+	
+	static HMETAFILE hMf;
+HDC hChildDc,hMetaDc;
+
+void DrawActorMetaFile(HDC hChildDc ,int lx,int ly)
+{
+	hMetaDc=CreateMetaFile(NULL);
+    	MoveToEx(hMetaDc,lx,ly+15,NULL);
+	LineTo(hMetaDc,lx,ly+30);
+	
+	MoveToEx(hMetaDc,lx,ly+22,NULL);
+	LineTo(hMetaDc,lx-20,ly+22);
+
+	MoveToEx(hMetaDc,lx,ly+22,NULL);
+	LineTo(hMetaDc,lx+20,ly+22);
+
+	MoveToEx(hMetaDc,lx,ly+30,NULL);
+	LineTo(hMetaDc,lx-20,ly+44);
+
+	MoveToEx(hMetaDc,lx,ly+30,NULL);
+	LineTo(hMetaDc,lx+20,ly+44);
+
+	Ellipse(hMetaDc,lx-15,ly-15,lx+15,ly+15);
+	hMf=CloseMetaFile(hMetaDc);
+	
+	PlayMetaFile(hChildDc,hMf);
+}
+
 
 LRESULT CALLBACK ChildProc(HWND hChildWnd,UINT iMsg,WPARAM wParam,LPARAM lParam)
 {
@@ -54,7 +90,7 @@ LRESULT CALLBACK ChildProc(HWND hChildWnd,UINT iMsg,WPARAM wParam,LPARAM lParam)
 							//}
 						}
 						
-						if(strcmp(tempID,"IDC_ACTHLINE")==0)
+						/*if(strcmp(tempID,"IDC_ACTHLINE")==0)
 						{
 							if(wParam & MK_LBUTTON) 
 							{
@@ -71,24 +107,8 @@ LRESULT CALLBACK ChildProc(HWND hChildWnd,UINT iMsg,WPARAM wParam,LPARAM lParam)
 							}
 							oldx=newx;
 							oldy=newy;
-						}
+						}*/
 
-
-						if(strcmp(tempID,"IDC_CMPTCIRCLE")==0 || strcmp(tempID,"IDC_UCELLIPSE")==0 || strcmp(tempID,"IDC_STELLIPSE")==0)
-						{
-							if(wParam & MK_LBUTTON) 
-							{
-								
-								SetROP2(hChildDc,/*R2_MASKPENNOT */ R2_NOTXORPEN);//R2_MERGEPENNOT);
-					
-								Ellipse(hChildDc,lx,ly-5,oldx,oldy);
-								Ellipse(hChildDc,lx,ly-5,newx,newy);
-
-								
-							}
-							oldx=newx;
-							oldy=newy;
-						}
 
 						if(strcmp(tempID,"IDC_CMPTCIRCLE")==0 || strcmp(tempID,"IDC_UCELLIPSE")==0 || strcmp(tempID,"IDC_STELLIPSE")==0)
 						{
@@ -130,11 +150,6 @@ LRESULT CALLBACK ChildProc(HWND hChildWnd,UINT iMsg,WPARAM wParam,LPARAM lParam)
 								
 								SetROP2(hChildDc,R2_NOTXORPEN);
 
-								MoveToEx(hChildDc,lx,ly,NULL);
-								LineTo(hChildDc,oldx,oldy);
-								
-								MoveToEx(hChildDc,oldx,oldy,NULL);
-								LineTo(hChildDc,oldx-25,oldy-25);
 
 								MoveToEx(hChildDc,oldx,oldy,NULL);
 								LineTo(hChildDc,oldx-25,oldy+25);
@@ -152,6 +167,41 @@ LRESULT CALLBACK ChildProc(HWND hChildWnd,UINT iMsg,WPARAM wParam,LPARAM lParam)
 							oldx=newx;
 							oldy=newy;
 						}
+						if(strcmp(tempID,"IDC_CMPTHLINE")==0 || strcmp(tempID,"IDC_ACTHLINE")==0 || strcmp(tempID,"IDC_STCLINE")==0 || strcmp(tempID,"IDC_UCCLINE")==0 || strcmp(tempID,"IDC_OBJCLINE")==0 || strcmp(tempID,"IDC_CBCLINE")==0)
+						{
+							if(wParam & MK_LBUTTON) 
+							{
+								
+								SetROP2(hChildDc,R2_NOT);
+
+								MoveToEx(hChildDc,lx,ly,NULL);
+								LineTo(hChildDc,oldx,oldy);
+								
+								MoveToEx(hChildDc,lx,ly,NULL);
+								LineTo(hChildDc,newx,newy);
+
+								
+							}
+							oldx=newx;
+							oldy=newy;
+						}
+						
+						if(strcmp(tempID,"IDC_ACTFCIRCLE")==0 || strcmp(tempID,"IDC_STFCIRCLE")==0)
+						{
+							if(wParam & MK_LBUTTON) 
+							{
+								
+								SetROP2(hChildDc,R2_NOT);
+
+								Ellipse(hChildDc,lx,ly,oldx,oldy);
+
+								Ellipse(hChildDc,lx,ly,newx,newy);
+							}
+							oldx=newx;
+							oldy=newy;
+						}
+
+
 						if(strcmp(tempID,"IDC_ACTDLINE")==0)
 						{
 							if(wParam & MK_LBUTTON) 
@@ -197,57 +247,6 @@ LRESULT CALLBACK ChildProc(HWND hChildWnd,UINT iMsg,WPARAM wParam,LPARAM lParam)
 							oldx=newx;
 							oldy=newy;
 						}
-						if(strcmp(tempID,"IDC_CMPTHLINE")==0 || strcmp(tempID,"IDC_ACTHLINE")==0 || strcmp(tempID,"IDC_STCLINE")==0 || strcmp(tempID,"IDC_UCCLINE")==0 || strcmp(tempID,"IDC_OBJCLINE")==0 || strcmp(tempID,"IDC_CBCLINE")==0)
-						{
-							if(wParam & MK_LBUTTON) 
-							{
-								
-								SetROP2(hChildDc,R2_NOT);
-
-								MoveToEx(hChildDc,lx,ly,NULL);
-								LineTo(hChildDc,oldx,oldy);
-								
-								MoveToEx(hChildDc,lx,ly,NULL);
-								LineTo(hChildDc,newx,newy);
-
-								
-							}
-							oldx=newx;
-							oldy=newy;
-						}
-						
-						if(strcmp(tempID,"IDC_ACTFCIRCLE")==0 || strcmp(tempID,"IDC_STFCIRCLE")==0)
-						{
-							if(wParam & MK_LBUTTON) 
-							{
-								
-								SetROP2(hChildDc,R2_NOT);
-
-								Ellipse(hChildDc,lx,ly,oldx,oldy);
-
-								Ellipse(hChildDc,lx,ly,newx,newy);
-							}
-							oldx=newx;
-							oldy=newy;
-						}
-
-						if(strcmp(tempID,"IDC_CMPTRECT")==0 || strcmp(tempID,"IDC_SQRECT")==0 || strcmp(tempID,"IDC_STRECT")==0 || strcmp(tempID,"IDC_UCRECT")==0 || strcmp(tempID,"IDC_OBJRECT")==0 || strcmp(tempID,"IDC_CBRECT")==0)
-						{
-							if(wParam & MK_LBUTTON) 
-							{
-								
-								SetROP2(hChildDc,R2_NOTXORPEN/*R2_MERGEPENNOT*/);//MERGEPEN);
-
-								Rectangle(hChildDc,lx,ly,oldx,oldy);
-
-								Rectangle(hChildDc,lx,ly,newx,newy);
-						
-							}
-							oldx=newx;
-							oldy=newy;
-						}
-
-						
 
 					   ReleaseDC(hChildWnd,hChildDc);
 
@@ -267,10 +266,15 @@ LRESULT CALLBACK ChildProc(HWND hChildWnd,UINT iMsg,WPARAM wParam,LPARAM lParam)
 	case WM_LBUTTONUP : //PlayMetaFile(GetDC(hChildWnd),hMf);
 						if(strcmp(tempID,"IDC_ACTHLINE")==0 || strcmp(tempID,"IDC_STCLINE")==0 || strcmp(tempID,"IDC_UCCLINE")==0 || strcmp(tempID,"IDC_OBJCLINE")==0 || strcmp(tempID,"IDC_CBCLINE")==0)						
 							SetRect(&lineRect[linecnt++],lx,ly,newx,newy);
-						
+						if(strcmp(tempID,"IDC_UCELLIPSE")==0 || strcmp(tempID,"IDC_STELLIPSE")==0)
+							
+						if(strcmp(tempID,"IDC_SQRECT")==0 || strcmp(tempID,"IDC_STRECT")==0 || strcmp(tempID,"IDC_UCRECT")==0 || strcmp(tempID,"IDC_OBJRECT")==0 || strcmp(tempID,"IDC_CBRECT")==0)
+							SetRect(&rectRect[rectcnt++],lx,ly,newx,newy);
+						if(strcmp(tempID,"IDC_ACTFCIRCLE")==0 || strcmp(tempID,"IDC_STFCIRCLE")==0)
+							SetRect(&fcircleRect[fcirclecnt++],lx,ly,newx,newy);
 						if(strcmp(tempID,"IDC_UCACTOR")==0)
 						{
-								DrawActorMetaFile(GetDC(hChildWnd),LOWORD(lParam),HIWORD(lParam));  
+							HIWORD(lParam));  
 								actorPt[actorcnt].x=LOWORD(lParam);
 								actorPt[actorcnt++].y =HIWORD(lParam);
 						}
@@ -284,7 +288,42 @@ LRESULT CALLBACK ChildProc(HWND hChildWnd,UINT iMsg,WPARAM wParam,LPARAM lParam)
 					 mii.fMask =MIIM_TYPE;
 					 mii.fType =MFT_STRING;
 
-					
+					 mii.wID =IDM_UNDO;
+					 mii.dwTypeData ="&Undo";
+					 mii.cch =8;
+					 InsertMenuItem(hSubMenu,0,1,&mii);
+					 mii.wID =IDM_REDO;
+					 mii.dwTypeData ="&Redo";
+					 mii.cch =8;
+					 InsertMenuItem(hSubMenu,1,1,&mii);
+					 
+					 mii.wID =IDM_CUT;
+					 mii.dwTypeData ="Cu&t";
+					 mii.cch =8;
+					 InsertMenuItem(hSubMenu,2,1,&mii);
+ 					 mii.wID =IDM_COPY;
+					 mii.dwTypeData ="&Copy";
+					 mii.cch =8;
+					 InsertMenuItem(hSubMenu,3,1,&mii);
+					 mii.wID =IDM_PASTE;
+					 mii.dwTypeData ="&Paste";
+					 mii.cch =8;
+					 InsertMenuItem(hSubMenu,4,1,&mii);
+					 mii.wID =IDM_DELETE;
+					 mii.dwTypeData ="&Delete";
+					 mii.cch =8;
+					 ii);
+
+					 mii.wID =IDM_REFRESH;
+					 mii.dwTypeData ="Re&fresh";
+					 mii.cch =8;
+					 InsertMenuItem(hSubMenu,7,1,&mii);
+
+					 mii.wID =IDM_PROPERTIES;
+					 mii.dwTypeData ="Prop&erties";
+					 mii.cch =8;
+					 InsertMenuItem(hSubMenu,8,1,&mii);
+
  					 mii.fMask =MIIM_SUBMENU|MIIM_TYPE;
 					 mii.dwTypeData ="Float";
 					 mii.cch =8;
@@ -301,6 +340,7 @@ LRESULT CALLBACK ChildProc(HWND hChildWnd,UINT iMsg,WPARAM wParam,LPARAM lParam)
 						MoveToEx(hChildDc,lineRect[i].left,lineRect[i].top,NULL);
 						LineTo(hChildDc,lineRect[i].right,lineRect[i].bottom);
 					}
+
 					for(i=0;i<rectcnt;i++)
 					{
 						Rectangle(hChildDc,rectRect[i].left ,rectRect[i].top ,rectRect[i].right ,rectRect[i].bottom );
@@ -325,40 +365,6 @@ LRESULT CALLBACK ChildProc(HWND hChildWnd,UINT iMsg,WPARAM wParam,LPARAM lParam)
 					DeleteObject(hBrush);
 					EndPaint(hChildWnd,&ps);
 					break;
-					
-					hBrush=(HBRUSH)GetStockObject(BLACK_BRUSH);
-					for(i=0;i<fcirclecnt;i++)
-					{
-						SelectObject(hChildDc,hBrush);
-						Ellipse(hChildDc,fcircleRect[i].left ,fcircleRect[i].top ,fcircleRect[i].right ,fcircleRect[i].bottom );
-					}
-					DeleteObject(hBrush);
-					EndPaint(hChildWnd,&ps);
-					break;
 	}
 	return DefWindowProc(hChildWnd,iMsg,wParam,lParam);
-}
-
-void DrawActorMetaFile(HDC hChildDc ,int lx,int ly)
-{
-	hMetaDc=CreateMetaFile(NULL);
-    MoveToEx(hMetaDc,lx,ly+15,NULL);
-	LineTo(hMetaDc,lx,ly+30);
-	
-	MoveToEx(hMetaDc,lx,ly+22,NULL);
-	LineTo(hMetaDc,lx-20,ly+22);
-
-	MoveToEx(hMetaDc,lx,ly+22,NULL);
-	LineTo(hMetaDc,lx+20,ly+22);
-
-	MoveToEx(hMetaDc,lx,ly+30,NULL);
-	LineTo(hMetaDc,lx-20,ly+44);
-
-	MoveToEx(hMetaDc,lx,ly+30,NULL);
-	LineTo(hMetaDc,lx+20,ly+44);
-
-	Ellipse(hMetaDc,lx-15,ly-15,lx+15,ly+15);
-	hMf=CloseMetaFile(hMetaDc);
-	
-	PlayMetaFile(hChildDc,hMf);
 }
