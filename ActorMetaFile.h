849ec61090a5c13d8913cc1a5768c5c8af82b6c9
crc442 <EequalsMCsquare91@gmail.com>
2014-04-27 11:34:24 -0500

    stati stuff

--- ActorMetaFile.h
+++ ActorMetaFile.h
@@ -5,3 +5,9 @@
 #include "StDlgProc.h"
 #include "ActDlgProc.h"
 #include "CmptDlgProc.h"
+
+
+static RECT lineRect[10];
+static RECT ellipseRect[10],fcircleRect[10];
+static RECT rectRect[10];
+static POINT actorPt[10];
