36c5682678bd376fe9e52938cc37e5f4bab7e2b9
crc442 <EequalsMCsquare91@gmail.com>
2014-04-28 11:34:24 -0500

    draw actor

--- ActorMetaFile.h
+++ ActorMetaFile.h
@@ -11,3 +11,24 @@ static RECT lineRect[10];
 static RECT ellipseRect[10],fcircleRect[10];
 static RECT rectRect[10];
 static POINT actorPt[10];
+
+oid DrawActorMetaFile(HDC hChildDc ,int lx,int ly)
+{
+	
+	MoveToEx(hMetaDc,lx,ly+22,NULL);
+	LineTo(hMetaDc,lx-20,ly+22);
+
+	MoveToEx(hMetaDc,lx,ly+22,NULL);
+	LineTo(hMetaDc,lx+20,ly+22);
+
+	MoveToEx(hMetaDc,lx,ly+30,NULL);
+	LineTo(hMetaDc,lx-20,ly+44);
+
+	MoveToEx(hMetaDc,lx,ly+30,NULL);
+	LineTo(hMetaDc,lx+20,ly+44);
+
+	Ellipse(hMetaDc,lx-15,ly-15,lx+15,ly+15);
+	hMf=CloseMetaFile(hMetaDc);
+	
+	PlayMetaFile(hChildDc,hMf);
+}
