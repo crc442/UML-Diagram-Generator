49da9068ff4ee2051eb073587ae05f4fe305ab93
crc442 <EequalsMCsquare91@gmail.com>
2014-05-03 15:14:23 -0500

    actor dialog

--- Adialog.h
+++ Adialog.h
@@ -3,12 +3,12 @@ BOOL CALLBACK ADlgProc(HWND hDlgWnd,UINT iMsg,WPARAM wParam,LPARAM lParam)
 	HWND hDlgItem;
 	switch(iMsg)
 	{
-		case WM_INITDIALOG : hDlgItem=GetDlgItem(hDlgWnd,ID_AOK);
+		case WM_INITDIALOG : hDlgItem=GetDlgItem(hDlgWnd);
 						     SetFocus (hDlgItem);
 						   break;
-		case WM_COMMAND : switch(LOWORD(wParam))
+		case WM_COMMAND : switch(LOWORD(wPairam))
 						  {
-								case ID_AOK : EndDialog(hDlgWnd,0);
+								case ID_AOK : EndDialog(hDlgWnd);
 											break;
 						  }
 						  break;
