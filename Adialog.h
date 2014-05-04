ad164cbf6c71a81f1804323642004e8ef04d7876
crc442 <EequalsMCsquare91@gmail.com>
2014-05-04 02:53:21 -0500

    actor err solved now

--- Adialog.h
+++ Adialog.h
@@ -3,12 +3,12 @@ BOOL CALLBACK ADlgProc(HWND hDlgWnd,UINT iMsg,WPARAM wParam,LPARAM lParam)
 	HWND hDlgItem;
 	switch(iMsg)
 	{
-		case WM_INITDIALOG : hDlgItem=GetDlgItem(hDlgWnd);
+		case WM_INITDIALOG : hDlgItem=GetDlgItem(hDlgWnd,ID_AOK);
 						     SetFocus (hDlgItem);
 						   break;
-		case WM_COMMAND : switch(LOWORD(wPairam))
+		case WM_COMMAND : switch(LOWORD(wParam))
 						  {
-								case ID_AOK : EndDialog(hDlgWnd, 0);
+								case ID_AOK : EndDialog(hDlgWnd,0);
 											break;
 						  }
 						  break;
