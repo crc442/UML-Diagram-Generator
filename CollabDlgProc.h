2072094fe84bfea301cb566d21ce582a7b5fb440
crc442 <EequalsMCsquare91@gmail.com>
2014-05-14 01:42:31 -0500

    collaborate dialog box

--- CollabDlgProc.h
+++ CollabDlgProc.h
@@ -1,29 +1,5 @@
 BOOL CALLBACK CollabDlgProc(HWND hcbDlgWnd,UINT iMsg,WPARAM wParam,LPARAM lParam) 
 {
 	HWND hBtn;
-	switch(iMsg)
-	{
-	case WM_INITDIALOG : hBtn=GetDlgItem(hcbDlgWnd,IDC_CBSELECT);
-						 SetFocus(hBtn);
-						 diaSelected=3;
-						break;
-		case WM_CLOSE : EndDialog(hcbDlgWnd,0);
-						CheckMenuItem(hMenu,IDM_TOOLBOX,MF_UNCHECKED);
-					    break;
-		case WM_COMMAND : switch(LOWORD(wParam))
-						  {
-							 
-							case IDC_CBFTLINE: strcpy(tempID,"IDC_CBFTLINE");
-												break;
-							case IDC_CBSELECT: strcpy(tempID,"IDC_CBSELECT");
-												break;
-							case IDC_CBRECT: strcpy(tempID,"IDC_CBRECT");
-												break;
-							case IDC_CBCLINE: strcpy(tempID,"IDC_CBCLINE");
-												break;
-						  }
-						  break;
-
-	}
-	return FALSE;
+	
 }
